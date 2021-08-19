61928e5dca83209f6324f40d39b3b35d9b795c9e
Marcos G <marcosgomesneto@gmail.com>
2021-08-19 09:02:52 -0300

    Update tags

--- woocommerce-pagarme-pix-payment.php
+++ woocommerce-pagarme-pix-payment.php
@@ -7,17 +7,19 @@
  * @package           WC_Pagarme_Pix_Payment
  *
  * @wordpress-plugin
- * Plugin Name:       Pix Automático com Pagarme para WooCommerce
- * Plugin URI:        https://github.com/marcosgomesneto/woocommerce-pagarme-pix-payment
- * Description:       Receba pagamentos via PIX no WooCommerce com a Pagar-me de forma automática sem precisar de comprovantes de pagamento.
- * Version:           1.0.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            Marcos Gomes Neto
- * Author URI:        https://github.com/marcosgomesneto
- * Text Domain:       wc-pagarme-pix-payment
- * License:           GPLv2 or later
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Plugin Name:       		Pix Automático com Pagarme para WooCommerce
+ * Plugin URI:        		https://github.com/marcosgomesneto/woocommerce-pagarme-pix-payment
+ * Description:       		Receba pagamentos via PIX no WooCommerce com a Pagar-me de forma automática sem precisar de comprovantes de pagamento.
+ * Version:           		1.0.0
+ * Requires at least: 		5.2
+ * Requires PHP:      		7.4
+ * WC requires at least:	3.0
+ * WC tested up to:      	4.9
+ * Author:            		Marcos Gomes Neto
+ * Author URI:        		https://github.com/marcosgomesneto
+ * Text Domain:       		wc-pagarme-pix-payment
+ * License:           		GPLv2 or later
+ * License URI:       		http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 defined( 'ABSPATH' ) || exit;
