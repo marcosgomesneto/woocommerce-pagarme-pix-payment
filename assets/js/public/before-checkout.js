9d9fd9d1b8758007be43df4335d3879ddf24b732
Marcos Gomes Neto <marcosgomesneto@gmail.com>
2022-06-21 23:55:20 -0300

    Update

--- assets/js/public/before-checkout.js
+++ assets/js/public/before-checkout.js
@@ -1,6 +1,5 @@
 jQuery(function ( $ ) {
 	'use strict';
-
   $('body').on("updated_checkout",function() {
     $('.payment_methods input[name=payment_method]').on('change', function ( e ) { 
       $('body').trigger( 'update_checkout' );
