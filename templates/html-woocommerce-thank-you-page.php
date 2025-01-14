4ea9739d756578dcffaf3f49ec70c49767d1c3d7
Leonardo <lgobatto@gmail.com>
2025-01-14 18:54:23 -0300

    Aumenta o número de tentativas para carregar o QR Code e ajusta o tempo de espera entre as tentativas

--- templates/html-woocommerce-thank-you-page.php
+++ templates/html-woocommerce-thank-you-page.php
@@ -23,7 +23,7 @@ $copy_button_html = ob_get_clean();
 ob_start();
 ?>
 <div id="qr-code-container" style="text-align: center;">
-    <div id="qr-code-loader" style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
+    <div id="qr-code-loader" style="display: flex; align-items: center; justify-content: center; flex-direction: column; padding: 16px;">
         <svg id="spinner" style="margin-bottom: 10px;" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 2.25V4.75M12 18V22M5.75 12H2.25M21.25 12H19.75M18.4571 18.4571L17.75 17.75M18.6642 5.41579L17.25 6.83M4.92157 19.0784L7.75 16.25M5.12868 5.20868L7.25 7.33" stroke="#1E2328" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
@@ -36,10 +36,8 @@ ob_start();
         const qrCodeImage = document.getElementById("qr-code-image");
         const loader = document.getElementById("qr-code-loader");
         const qrCodeUrl = "<?php echo esc_js($qr_code_image); ?>";
-        const maxAttempts = 5;
+        const maxAttempts = 15;
         let attemptCount = 0;
-
-        console.log("QR Code URL: " + qrCodeUrl);
 
         function checkImage(url, callback) {
             const img = new Image();
@@ -69,9 +67,9 @@ ob_start();
                     loader.style.display = "none";
                     console.log("QR Code image displayed.");
                 } else {
-                    console.log("QR Code image not available, retrying in 2 seconds...");
+                    console.log("QR Code image not available, retrying in 3 seconds...");
                     attemptCount++;
-                    setTimeout(loadQrCode, 2000); // Tenta novamente após 2 segundos
+                    setTimeout(loadQrCode, 3000); // Tenta novamente após 3 segundos
                 }
             });
         }
